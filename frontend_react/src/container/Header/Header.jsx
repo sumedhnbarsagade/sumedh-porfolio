c47042ad381dcf6391f31d937c89e5243d28a6ad
sbarsagade248@gmail.com <sbarsagade248@gmail.com>
2024-10-01 17:04:53 +0530

    header files created

--- frontend_react/src/container/Header/Header.jsx
+++ frontend_react/src/container/Header/Header.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
-import { motion } from 'framer-motion'
+import { motion, transform } from 'framer-motion'
 import { images } from '../../constants'
 
 
 import './Header.scss'
 
+const scaleVariants = {
+  whileInView: {
+    scale: [0, 1],
+    opacity: [0, 1],
+    transition: {
+      duration: 1,
+      ease: 'easeInOut'
+    }
+  }
+}
+
 function Header() {
   return (
-    <div className='app__header app__flex'>
+    <div id='home' className='app__header app__flex'>
       <motion.div
       whileInView={{x: [-100, 0], opacity: [0,1]}}
       transition={{duration: 0.5}}
@@ -22,11 +33,38 @@ function Header() {
               </div>
             </div>
             <div className='tag-cmp app__flex'>
-              <p className='p-text'>Web Developer</p>
+              <p className='p-text'>Web Developer | </p>
               <p className='p-text'>React Js</p>
 
             </div>
         </div>
+      </motion.div>
+      <motion.div
+       whileInView={{x: [-100, 0], opacity: [0,1]}}
+       transition={{duration: 0.5}}
+       className='app__header-img'
+       >
+       <img src={images.profile} alt="profile-bg"/>
+       <motion.img
+       whileInView={{scale: [0, 1]}}
+       transition={{duration: 1, ease: 'easeInOut'}}
+       src={images.circle}
+       alt='profile_circle'
+       className="overlay_circle"
+       />
+      </motion.div>
+
+      <motion.div
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className='app__header-circles'
+      >
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          <div className='circle-cmp app__flex' key={`circle-${index}`}>
+            <img src={circle} alt='circle'/>
+          </div>
+        ))}
+
       </motion.div>
     </div>
   )
